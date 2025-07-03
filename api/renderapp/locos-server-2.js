aa4e642a135623f18aa94644f3fdce044efdb62c
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2025-07-03 16:03:04 -0500

    check renderapp status

--- api/renderapp/locos-server-2.js
+++ api/renderapp/locos-server-2.js
@@ -1,23 +1,15 @@
-function waitRenderAppInit(status_url) {
-  return new Promise(resolve => {
-    const interval = setInterval(async () => {
-      const res = await fetch(status_url).then(res => res.text())
-      if (res == "True" || res == "False") {
-        resolve(res)
-        clearInterval(interval)
-      }
-      console.log("Waiting render app");
-    }, 5000)
-  })
+async function get_renderapp_status(status_url) {
+  try {
+    await fetch(status_url).then(res => res.json())
+    return true
+  } catch (error) {
+    return false 
+  }
 }
 
 export default async function handler(req, res) {
-  const status_url = "https://minecraft-skin-changer.onrender.com/server/administration/schedule/check/status/"
-  let status = await fetch(status_url).then(res => res.text())
-
-  if (status != "True" && status != "False") {
-    status = await waitRenderAppInit(status_url)
-  }
+  const status_url = "https://minecraft-skin-changer.onrender.com/status/"
+  const status = await get_renderapp_status(status_url)
 
   return res.status(200).send(status)
 }
