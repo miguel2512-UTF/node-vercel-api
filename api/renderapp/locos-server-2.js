2d13bf1a259bcefd7860e67b6d8749c2b21e900e
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2025-07-03 19:23:02 -0500

    get render app status

--- api/renderapp/locos-server-2.js
+++ api/renderapp/locos-server-2.js
@@ -1,15 +1,19 @@
-async function get_renderapp_status(status_url) {
-  try {
-    await fetch(status_url).then(res => res.json())
-    return true
-  } catch (error) {
-    return false 
-  }
+async function get_render_app_status(status_url, timeout = 1000) {
+    const controller = new AbortController()
+    const idTimeout = setTimeout(() => controller.abort(), timeout)
+
+    try {
+        await fetch(status_url, { signal: controller.signal })
+        clearTimeout(idTimeout)
+        return true
+    } catch (error) {
+        return false
+    }
 }
 
 export default async function handler(req, res) {
   const status_url = "https://minecraft-skin-changer.onrender.com/status/"
-  const status = await get_renderapp_status(status_url)
+  const status = await get_render_app_status(status_url, 2000)
 
   return res.status(200).send(status)
 }
